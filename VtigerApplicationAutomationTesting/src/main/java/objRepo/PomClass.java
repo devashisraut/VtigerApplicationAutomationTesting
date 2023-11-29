0a0824774f139d8b99661a218a907575751663a9
Devashis <Devashis@Devashis.mshome.net>
2023-11-29 12:07:34 +0530

    change

--- VtigerApplicationAutomationTesting/src/main/java/objRepo/PomClass.java
+++ VtigerApplicationAutomationTesting/src/main/java/objRepo/PomClass.java
@@ -3,6 +3,6 @@ package objRepo;
 public class PomClass {
 	public void add() {
 		System.out.println("hi...");
-
+		System.out.println("hello");
 	}
 }
---
+++ VtigerApplicationAutomationTesting/src/main/java/objRepo/PullClass.java
@@ -0,0 +1,7 @@
+package objRepo;
+
+public class PullClass {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
